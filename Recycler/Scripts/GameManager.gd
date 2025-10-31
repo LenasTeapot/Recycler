de507108aaf808cdb24d4e1a9dc081adccf6a989
LenasTeapot <lenaguinn@gmail.com>
2025-10-30 21:26:46 -0700

    Adding background music

--- Recycler/Scripts/GameManager.gd
+++ Recycler/Scripts/GameManager.gd
@@ -1,5 +1,11 @@
 extends Node3D
 	
+@onready var audio_bg : AudioStreamPlayer = self.find_child("AudioStreamPlayer")
+	
+func _ready():
+	if audio_bg != null and audio_bg.stream != null:
+		audio_bg.play()
+	
 func _input(event):
 	if Input.is_action_just_pressed("Quit"):
 		get_tree().quit()
