18f3dee28838883c83638f71c64ca516cbf8ce62
LenasTeapot <lenaguinn@gmail.com>
2026-06-23 10:02:04 -0700

    Trash Pick Up Interaction obsolete

--- Recycler/Scripts/GameManager.gd
+++ Recycler/Scripts/GameManager.gd
@@ -13,7 +13,7 @@ func _ready():
 	if audio_bg != null and audio_bg.stream != null:
 		audio_bg.play()
 	
-func _input(event):
+func _input(_event):
 	if Input.is_action_just_pressed("Quit"):
 		get_tree().quit()
 	if Input.is_action_just_pressed("Cheat"):
